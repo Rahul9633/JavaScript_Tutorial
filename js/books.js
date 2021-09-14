8688a2a5ea020bb0c953515f8d180d381df1b028
rahul9633 <rahulkumar.rexweb@gmail.com>
2021-09-14 18:53:23 +0530

    second commit

---
+++ js/books.js
@@ -0,0 +1,83 @@
+var Books = [{ 'title': 'RD sharma', 'price': 123, 'author': 'RD', 'publisher': 'Arihant' }]
+
+function BindBooks(books) {
+    var row = `<tr>
+    <th>S.no</th>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Author</th>
+    <th>Publication</th>
+    <th>Action</th>
+</tr>`;
+    books.forEach(function (book, i) {
+        row += `<tr><td>${i + 1}</td><td>${Books[i].title}</td><td>${Books[i].price}</td><td>${Books[i].author}</td><td>${Books[i].publisher}</td><td><a href="#" onclick=edit(${i})>Edit</a><br><a href="#" onclick=del(${i})>Delete</a></td></tr>`
+    })
+
+    document.getElementById("tbl").innerHTML = row;
+}
+
+function edit(i) {
+    var book = Books[i];
+    document.getElementById("title").value = Books[i].title
+    document.getElementById("price").value = Books[i].price
+    document.getElementById("author").value = Books[i].author
+    document.getElementById("publish").value = Books[i].publisher
+    document.getElementById("index").value = i;
+    document.getElementById("btn-update").setAttribute("onclick", "update(" + i + ")");
+
+
+}
+
+function del(i) {
+    // event.preventDefault();
+    Books.splice(i, 1);
+    BindBooks(Books);
+}
+
+
+function AddBook() {
+    var title = document.getElementById("title").value;
+    var price = document.getElementById("price").value;
+    var author = document.getElementById("author").value;
+    var publisher = document.getElementById("publish").value;
+    if (title !== "" && price !== "" && author != "" && publisher != "") {
+        Books.push({ "title": title, "price": price, "author": author, "publisher": publisher })
+    }
+
+
+
+
+    BindBooks(Books);
+    ClearForm();
+
+
+}
+
+
+
+function update(i) {
+    console.log(i);
+    var title = document.getElementById("title").value;
+    var price = document.getElementById("price").value;
+    var author = document.getElementById("author").value;
+    var publisher = document.getElementById("publish").value;
+    Books[i].title = title;
+    Books[i].price = price;
+    Books[i].author = author;
+    Books[i].publisher = publisher;
+
+    BindBooks(Books);
+    ClearForm();
+
+}
+
+
+function ClearForm() {
+
+    document.getElementById("title").value = ""
+    document.getElementById("price").value = ""
+    document.getElementById("author").value = ""
+    document.getElementById("publish").value = ""
+
+}
+BindBooks(Books);
